c5de84c22a494901d65d056c874f3721ae060df3
Humper04 <julianmensink25@gmail.com>
2024-10-14 13:08:54 +0200

    Removed 2fa
    Zipped project and removed 2fa and dot net from project

--- registratie.php
+++ registratie.php
@@ -1,14 +1,9 @@
 <?php
-require '/var/www/vendor/autoload.php';
-
-$dotenv = \Dotenv\Dotenv::createImmutable('/var/www/');
-$dotenv->load();
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Database connectie
     $servername = "localhost";
     $username = "root";
-    $password = $_ENV['DB_PASSWORD_ROOT'];
+    $password = "8BF54eq$%gpXTBZ4";
     $database = "project5";
 
     $conn = new mysqli($servername, $username, $password, $database);
@@ -20,8 +15,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Haal de gegevens op uit het formulier
     $gebruikersnaam = $_POST['gebruikersnaam'];
     $wachtwoord = password_hash($_POST['wachtwoord'], PASSWORD_DEFAULT);
-    $mfa_choice = strtolower(trim($_POST['registreren_met_mfa'])); // Zet naar lowercase en trim spaties
-
     // Voeg de gebruiker toe aan de database
     $sql = "INSERT INTO gebruikers (gebruikersnaam, wachtwoord) VALUES (?, ?)";
     $stmt = $conn->prepare($sql);
@@ -33,19 +26,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $_SESSION['gebruiker_id'] = $stmt->insert_id;
         $_SESSION['gebruikersnaam'] = $gebruikersnaam;
 
-        // Controleer de keuze voor MFA (j of n)
-        if (isset($_POST['submit_mfa'])) {
-            echo "<script>console.log('Keuze: *" . $mfa_choice . "*');</script>"; // Log de keuze naar de console
-        } elseif ($mfa_choice === 'j') {
-            $_SESSION['user_logged_in'] = 1;
-            echo "<script>console.log('Keuze: *" . $mfa_choice . "*');</script>"; // Log de keuze naar de console
-            header("Location: setup_mfa.php"); // Stuur door naar setup_mfa.php als 'j' is geselecteerd
-            // exit();
-        } else {
-            echo "<script>console.log('Keuze: *n*');</script>"; // Log de keuze naar de console
-            header("Location: login.php"); // Anders naar de loginpagina
-            // exit();
-        }
     } else {
         echo "Fout bij het registreren van de gebruiker. Probeer het later opnieuw.";
     }
@@ -73,10 +53,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <div class="form-group">
                 <label for="wachtwoord">Wachtwoord</label>
                 <input type="password" class="form-control" id="wachtwoord" name="wachtwoord" required>
-            </div>
-            <div class="form-group">
-                <label for="registreren_met_mfa">[j/N] Registreren met MFA</label>
-                <input type="text" class="form-control" id="registreren_met_mfa" name="registreren_met_mfa" maxlength="1" pattern="[jnJN]?" title="Voer j of n in" placeholder="j of n">
             </div>
             <button type="submit" name="submit_mfa" class="btn btn-secondary">Toon keuze in console</button>
             <button type="submit" class="btn btn-primary">Registreren</button>
